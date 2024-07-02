d53a7c3443fa6d1aa81a5b6cc1af6e9a410cbd60
Robin Sabhaya <robin47ppsv@gmail.com>
2024-07-02 21:55:25 +0530

    Create API for verify token

--- src/routes/v1/commonRoutes/auth.route.js
+++ src/routes/v1/commonRoutes/auth.route.js
@@ -2,12 +2,14 @@ const express = require('express');
 const validate = require('../../../middlewares/validate');
 const authValidation = require('../../../validations/auth.validation');
 const authController = require('../../../controllers/commonControllers/auth.controller');
-const auth = require('../../../middlewares/auth');
+const { authorizeV3 } = require('../../../middlewares/auth');
+const { ROLES } = require('../../../helper/constant.helper');
 
 const router = express.Router();
 
 router.post('/social-login', validate(authValidation.socialLogin), authController.socialLogin);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/register', validate(authValidation.register), authController.register);
+router.get('/verify-token', authorizeV3(ROLES.user), authController.verifyToken);
 
 module.exports = router;
